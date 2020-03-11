24ea6ddc618d8dec7813108512c49f5d0de13b62
toasterkief <toasterkief@localhost.localdomain>
2020-03-10 21:32:44 -0500

    User registration

--- app/routes.py
+++ app/routes.py
@@ -1,44 +1,17 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
-
-
-# @app.route('/')
-# @app.route('/index')
-# def index():
-    # return "Welcome to the Order"
-'''
-
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'toasterkief'}
-    return render_template('index.html', title='The Great Hall', user=user)
-
-
-@app.route('/index-w-posts')
-def index_posts():
-    user = {'username': 'toasterkief'}
-    posts = [
-        {
-            'author': {'username': 'Zodin'},
-            'body': 'Lorem ipsum dolor sit amet, consectetur adipisic'
-        },
-        {
-            'author': {'username': "Michael Landon"},
-            'body': """"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim """
-        }
-    ]
-    return render_template('index_w_posts.html', title='Round Table', user=user, posts=posts)
-
-'''
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'toasterkief'}
+    #user = {'username': 'toasterkief'}
     posts = [
         {
             'author': {'username': 'Zodin'},
@@ -49,16 +22,9 @@ def index():
             'body': 'But I will hinder thee as I may!'
         }
     ]
-    return render_template('index.html', title='The Echo Halls', user=user, posts=posts)
+    return render_template('index.html', title='The Echo Halls', posts=posts)
 
-
-"""
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
-"""
-
+'''
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -66,3 +32,42 @@ def login():
         flash("Login requested for user {}, remember me={}".format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Log In', form=form)
+'''
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Welcome comrade, to the Order!")
+        return redirect(url_for('login'))
+    return render_template('register.html', title="Join Us", form=form)
